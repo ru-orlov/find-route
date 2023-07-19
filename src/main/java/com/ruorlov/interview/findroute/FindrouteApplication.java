d16805bd4a659e781735ebda5d42ad4eec56acc3
ru-orlov <ruorlov@gmail.com>
2023-07-19 19:45:05 +0200

    - init commit

---
+++ src/main/java/com/ruorlov/interview/findroute/FindrouteApplication.java
@@ -0,0 +1,13 @@
+package com.ruorlov.interview.findroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindrouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruorlov/interview/findroute/FindrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruorlov.interview.findroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
