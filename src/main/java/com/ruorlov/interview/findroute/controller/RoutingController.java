28adf46202ef0f91df8547c3ed4f7515eb1fefe2
ru-orlov <ruorlov@gmail.com>
2023-07-23 11:19:29 +0200

    - Tidy up - extend README.md

--- src/main/java/com/ruorlov/interview/findroute/controller/RoutingController.java
+++ src/main/java/com/ruorlov/interview/findroute/controller/RoutingController.java
@@ -1,6 +1,8 @@
 package com.ruorlov.interview.findroute.controller;
 
 import com.ruorlov.interview.findroute.service.RoutingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +13,13 @@ import java.util.List;
 @RestController
 @RequestMapping("routing")
 public class RoutingController  {
-
     private final RoutingService routingService;
-
     public RoutingController(RoutingService routingService) {
         this.routingService = routingService;
     }
-
     @GetMapping("/{origin}/{destination}")
-    public List<String> getPath(@PathVariable String origin, @PathVariable String destination) {
-        return routingService.route(origin, destination);
+    public ResponseEntity<List<String>> getPath(@PathVariable String origin, @PathVariable String destination) {
+        return new ResponseEntity<>(routingService.route(origin, destination), HttpStatus.OK);
     }
 
 }
